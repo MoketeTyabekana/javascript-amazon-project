1ec7c9db56bcaa9da49763820349f6abb16d7aa3
Mokete <ematyabekana@gmail.com>
2025-06-08 14:58:47 +0200

    Fix data attribute for "Add to Cart" button and update cart logic to use product ID

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,8 +40,8 @@ products.forEach((product) => {
        <img src="images/icons/checkmark.png">
        Added
      </div>
-     <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${
-       product.name
+     <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+       product.id
      }" >
        Add to Cart
      </button>
@@ -52,12 +52,13 @@ document.querySelector(".products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productName = button.dataset.productName;
+
+    const productId = button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-      if (productName === item.productName) {
+      if (productId === item.productId) {
         matchingItem = item;
       }
     });
@@ -67,7 +68,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     } 
     else {
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1, // Default quantity
       });
       console.log(cart);
